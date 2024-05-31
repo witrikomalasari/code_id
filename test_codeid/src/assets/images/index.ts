fdb15f0ba37b8594a0571fc11d6bfc979433e3a1
witrikomalasari <dede.witri@gmail.com>
2024-05-31 18:16:59 +0700

    update: add images

---
+++ test_codeid/src/assets/images/index.ts
@@ -0,0 +1,12 @@
+export const images = {
+  coffeeMiddle: require('./coffeMiddle.png'),
+  coffee2: require('./coffe2.png'),
+  coffeeSmall: require('./coffeSmall.png'),
+  coffeeSplash: require('./coffeMain.png'),
+  coffeeLarge: require('./coffeLarge.png'),
+  user1: require('./user1.png'),
+  user2: require('./user2.png'),
+  coffeeRoast: require('./coffeRoast.png'),
+  milk: require('./milk.png'),
+  setting: require('./setting.png'),
+};
